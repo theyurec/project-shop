fafaf58041eb2b7345d7d882ffa018d4261b7e0d
Yuriy <the.yurec1@yandex.ru>
2022-07-09 16:38:39 +0300

    fonts and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css'
 import './assets/styles.css'
 import React from 'react'
-import './pages/components/app/Layout/Header/index'
+import './pages/components/app/Layout/Header'
 
 // Роутинг компонентов
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <span className="hello1">Hello1</span>
+      <span className="hello2">Hello2</span>
+      <span className="hello3">Hello3</span>
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/components/app/Layout/Header/index.js
@@ -0,0 +1,8 @@
+import styles from './Header.module.scss'
+import React from 'react'
+
+const Index = () => {
+  return <div className={styles.green}>111</div>
+}
+
+export default Index
