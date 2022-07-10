6cf553e77baeb32354c27d28a4f42d1fc6bc90fc
Yuriy <the.yurec1@yandex.ru>
2022-07-10 11:52:59 +0300

    header

--- src/components/app/Layout/Header/index.jsx
+++ src/components/app/Layout/Header/index.jsx
@@ -3,7 +3,21 @@ import { mainMenu } from '../../../../libs/mainMenu'
 import styles from './Header.module.scss'
 
 const Header = () => {
-  return <div className={styles.green}>{mainMenu.map((item) => item.name)}</div>
+  return (
+    <>
+      <div className={styles.header}>
+        <div className={styles.menu}>
+          {mainMenu.map((item, id) => {
+            return (
+              <div className={styles.item} key={id}>
+                {item.name}
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Header
--- src/libs/mainMenu.js
+++ src/libs/mainMenu.js
@@ -1,4 +1,19 @@
 export const mainMenu = [
-  { name: 'Main', link: '/' },
-  { name: 'Catalog', link: '/catalog', children: [{ name: 'Накладные электронные замки', link: '/catalog/1' }] }
+  { name: 'Главная', link: '/' },
+  {
+    name: 'Каталог',
+    link: '/catalog',
+    children: [
+      { name: 'Накладные электронные замки', link: '/catalog/1' },
+      { name: 'Врезные электронные замки', link: '/catalog/1' },
+      { name: 'Замки для квартиры', link: '/catalog/1' },
+      { name: 'Замки для дома', link: '/catalog/1' },
+      { name: 'Замки для отелей', link: '/catalog/1' },
+      { name: 'Замки для офиса', link: '/catalog/1' },
+      { name: 'Замки для шкафчиков', link: '/catalog/1' },
+      { name: 'Замки для раздевалок', link: '/catalog/1' }
+    ]
+  },
+  { name: 'Оптовая продажа', link: '/' },
+  { name: 'О нас', link: '/' }
 ]
