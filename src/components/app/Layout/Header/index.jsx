1ef065e81a29a8ec686df554d60370c45e854813
Yuriy <the.yurec1@yandex.ru>
2022-07-13 18:06:36 +0300

    add logo and menu

--- src/components/app/Layout/Header/index.jsx
+++ src/components/app/Layout/Header/index.jsx
@@ -1,17 +1,46 @@
 import React from 'react'
 import { mainMenu } from '../../../../libs/mainMenu'
 import styles from './Header.module.scss'
+// import { useState } from 'react'
+import logo from '../../../../assets/images/logo.svg'
+import down_arrow from '../../../../assets/images/down_arrow.svg'
 
 const Header = () => {
+  // const [click, setClick] = useState(false)
+  // const list = () => {
+  //   setClick(!click)
+  // }
+
   return (
     <>
       <div className={styles.header}>
+        <img src={logo} alt="logo" className={styles.logo} />
         <div className={styles.menu}>
-          {mainMenu.map((item, id) => {
+          {mainMenu.map((item, index) => {
             return (
-              <div className={styles.item} key={id}>
-                {item.name}
-              </div>
+              <ul key={index}>
+                <li className={styles.item}>
+                  {item.name}
+
+                  {/*{item.children !== undefined && <img onMouseEnter={list} onMouseLeave={list} src={down_arrow} alt="arrow" />}*/}
+                  {/*{click && item.children !== undefined && (*/}
+                  {/*  <div>*/}
+                  {/*    {item.children.map((item, index) => {*/}
+                  {/*      return <li key={index}>{item.name}</li>*/}
+                  {/*    })}*/}
+                  {/*  </div>*/}
+                  {/*)}*/}
+
+                  {item.children !== undefined && <img className={styles.arrow} src={down_arrow} alt="arrow" />}
+                  {item.children.map((item, index) => {
+                    return (
+                      <ul key={index}>
+                        <li>{item.name}</li>
+                      </ul>
+                    )
+                  })}
+                </li>
+              </ul>
             )
           })}
         </div>
