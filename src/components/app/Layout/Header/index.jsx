fbbaf149e0af60e1463965f268e70dceb104e086
Yuriy <the.yurec1@yandex.ru>
2022-07-16 21:31:35 +0300

    logo and styles menu

--- src/components/app/Layout/Header/index.jsx
+++ src/components/app/Layout/Header/index.jsx
@@ -1,49 +1,42 @@
 import React from 'react'
 import { mainMenu } from '../../../../libs/mainMenu'
 import styles from './Header.module.scss'
-// import { useState } from 'react'
 import logo from '../../../../assets/images/logo.svg'
 import down_arrow from '../../../../assets/images/down_arrow.svg'
 
 const Header = () => {
-  // const [click, setClick] = useState(false)
-  // const list = () => {
-  //   setClick(!click)
-  // }
-
   return (
     <>
       <div className={styles.header}>
         <img src={logo} alt="logo" className={styles.logo} />
-        <div className={styles.menu}>
+        <ul className={styles.menu}>
           {mainMenu.map((item, index) => {
             return (
-              <ul key={index}>
-                <li className={styles.item}>
-                  {item.name}
-
-                  {/*{item.children !== undefined && <img onMouseEnter={list} onMouseLeave={list} src={down_arrow} alt="arrow" />}*/}
-                  {/*{click && item.children !== undefined && (*/}
-                  {/*  <div>*/}
-                  {/*    {item.children.map((item, index) => {*/}
-                  {/*      return <li key={index}>{item.name}</li>*/}
-                  {/*    })}*/}
-                  {/*  </div>*/}
-                  {/*)}*/}
-
-                  {item.children !== undefined && <img className={styles.arrow} src={down_arrow} alt="arrow" />}
-                  {item.children.map((item, index) => {
-                    return (
-                      <ul key={index}>
-                        <li>{item.name}</li>
-                      </ul>
-                    )
-                  })}
-                </li>
-              </ul>
+              <li key={index} className={styles.item}>
+                {item.name}
+                {!!item.children && <img className={styles.arrow} src={down_arrow} alt="arrow" />}
+                {!!item.children && (
+                  <>
+                    <div className={styles.submenu_margin}></div>
+                    <ul className={styles.submenu_list}>
+                      {item.children.map((item, index) => {
+                        return (
+                          <>
+                            <li className={styles.submenu} key={index}>
+                              <a href="#">{item.name}</a>
+                              <div className={styles.submenu_line}></div>
+                            </li>
+                          </>
+                        )
+                      })}
+                      <button className={styles.btn_submenu}>Смотерть все</button>
+                    </ul>
+                  </>
+                )}
+              </li>
             )
           })}
-        </div>
+        </ul>
       </div>
     </>
   )
