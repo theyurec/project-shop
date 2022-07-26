b82e6388bd809f20be3ccba59b940ce57e24860a
Yuriy <the.yurec1@yandex.ru>
2022-07-26 16:29:47 +0300

    add mobile footer

--- src/components/app/Layout/index.jsx
+++ src/components/app/Layout/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Header from './Header'
+// import Header from './Header'
 import Footer from './Footer'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <Router>
-      <Header />
+      {/*<Header />*/}
       <Footer />
     </Router>
   )
